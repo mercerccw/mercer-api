5bf157939963b2b6569f1334bce20b053b93a5d2
Clayton Mercer <mercerccw@gmail.com>
2021-06-15 19:34:09 -0400

    Initial Commit

---
+++ src/main/java/com/mercersystems/mercerapi/MercerApiApplication.java
@@ -0,0 +1,13 @@
+package com.mercersystems.mercerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercersystems/mercerapi/MercerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercersystems.mercerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
